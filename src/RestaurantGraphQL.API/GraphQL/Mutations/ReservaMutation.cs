3f28820e371db6840e437da01c5d84b99abfaa98
Fabio Borges <fabio.nascimento@serafin.com.br>
2025-09-02 05:00:17 -0300

    .

--- src/RestaurantGraphQL.API/GraphQL/Mutations/ReservaMutation.cs
+++ src/RestaurantGraphQL.API/GraphQL/Mutations/ReservaMutation.cs
@@ -2,23 +2,39 @@
 using RestaurantGraphQL.Core.Interfaces.Repositories;
 using RestaurantGraphQL.Core.Models;
 
-namespace RestaurantGraphQL.API.GraphQL.Mutations
-{
-    [ExtendObjectType("Mutation")]
-    public class ReservaMutation
-    {
-        public async Task<Reserva> AddReserva(ReservaInput input, [Service] IReservaRepository repository)
-        {
-            var reserva = new Reserva
-            {
-                DataHora = DateTime.Now,
-                NomeCliente = input.NomeCliente,
-                NumeroPessoas = input.NumeroPessoas,
-                Observacoes = input.Observacoes
-            };
+namespace RestaurantGraphQL.API.GraphQL.Mutations;
 
-            await repository.Add(reserva);
-            return reserva;
-        }
+[ExtendObjectType("Mutation")]
+public class ReservaMutation
+{
+    public async Task<Reserva> AddReserva(ReservaInput input, [Service] IReservaRepository repository)
+    {
+        var reserva = new Reserva
+        {
+            DataHora = DateTime.Now,
+            NomeCliente = input.NomeCliente,
+            NumeroPessoas = input.NumeroPessoas,
+            Observacoes = input.Observacoes
+        };
+
+        await repository.Add(reserva);
+        return reserva;
+    }
+
+    public async Task<Reserva?> UpdateReserva(int id, ReservaInput input, [Service] IReservaRepository repository)
+    {
+        var reserva = new Reserva
+        {
+            NomeCliente = input.NomeCliente,
+            NumeroPessoas = input.NumeroPessoas,
+            Observacoes = input.Observacoes
+        };
+
+        return await repository.Update(id, reserva);
+    }
+
+    public async Task<bool> DeleteReserva(int id, [Service] IReservaRepository repository)
+    {
+        return await repository.Delete(id);
     }
 }
